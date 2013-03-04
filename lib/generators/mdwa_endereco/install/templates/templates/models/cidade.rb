7bed50b62ebab4229b65c6b43fd0f0328d4b447d
Marcelo Theodoro <contato@qw3.com.br>
2013-03-04 08:32:16 -0300

    nome completo para model cidade.

--- lib/generators/mdwa_endereco/install/templates/templates/models/cidade.rb
+++ lib/generators/mdwa_endereco/install/templates/templates/models/cidade.rb
@@ -1,12 +1,14 @@
 # -*- encoding : utf-8 -*-
-
 class A::Cidade < ActiveRecord::Base
 
-    attr_accessible :nome, :latitude, :longitude, :populacao, :estado
+  attr_accessible :nome, :latitude, :longitude, :populacao, :estado
 
-  
-    has_many :enderecos, :class_name => 'A::Endereco'
-    belongs_to :estado, :class_name => 'A::Estado'
-    attr_accessible :estado_id
+  has_many :enderecos, :class_name => 'A::Endereco'
+  belongs_to :estado, :class_name => 'A::Estado'
+  attr_accessible :estado_id
+
+  def nome_completo
+    "#{nome} - #{estado.acronimo.upcase}"
+  end
   
 end
